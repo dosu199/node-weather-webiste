4515fa74c22f26fcbfa21d901e08cf9aeca36bc8
dosu199 <58472953+dosu199@users.noreply.github.com>
2020-09-18 14:50:40 +0200

    Added humidity paramterer to app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         else if (body.error)
             callback("Unable to find location", undefined)
         else {
-            callback(undefined,`${body.current.weather_descriptions[0]}. Current temaperature is ${body.current.temperature} and feelslike ${body.current.feelslike}`)
+            callback(undefined,`${body.current.weather_descriptions[0]}. Current temaperature is ${body.current.temperature} but feelslike ${body.current.feelslike} and humidity is ${body.current.humidity}`)
         }
     })
 }
